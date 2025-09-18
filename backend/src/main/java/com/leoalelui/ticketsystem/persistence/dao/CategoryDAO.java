000a09c9d2a5c1028f579f2c4912221d90de44e8
alej0nt <alej0nt666@gmail.com>
2025-09-17 19:26:18 -0500

    Hecha logica de negocio sin probar de categorias

--- backend/src/main/java/com/leoalelui/ticketsystem/persistence/dao/CategoryDAO.java
+++ backend/src/main/java/com/leoalelui/ticketsystem/persistence/dao/CategoryDAO.java
@@ -1,6 +1,7 @@
 package com.leoalelui.ticketsystem.persistence.dao;
 
 import com.leoalelui.ticketsystem.domain.dto.request.CategoryCreateDTO;
+import com.leoalelui.ticketsystem.domain.dto.request.CategoryUpdateDTO;
 import com.leoalelui.ticketsystem.domain.dto.response.CategoryResponseDTO;
 import com.leoalelui.ticketsystem.persistence.entity.CategoryEntity;
 import com.leoalelui.ticketsystem.persistence.mapper.CategoryMapper;
@@ -26,5 +27,24 @@ public class CategoryDAO {
 
     public boolean existsById(Long id) {
         return categoryRepository.existsById(id);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+    public boolean existsByName(String categoryName) {
+        return categoryRepository.existsByName(categoryName);
+    }
+
+    public CategoryResponseDTO update(Long id, CategoryUpdateDTO categoryUpdateDTO) {
+        CategoryEntity old = categoryRepository.findById(id).get();
+        old.setName(categoryUpdateDTO.getName());
+        old.setDescription(categoryUpdateDTO.getDescription());
+        return categoryMapper.toResponseDTO(categoryRepository.save(old));
+    }
+
+    public CategoryResponseDTO getByName(String categoryName) {
+        return categoryMapper.toResponseDTO(categoryRepository.findByName(categoryName));
     }
 }
--- backend/src/main/java/com/leoalelui/ticketsystem/persistence/repository/CategoryRepository.java
+++ backend/src/main/java/com/leoalelui/ticketsystem/persistence/repository/CategoryRepository.java
@@ -3,4 +3,7 @@ package com.leoalelui.ticketsystem.persistence.repository;
 import com.leoalelui.ticketsystem.persistence.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {}
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    boolean existsByName(String name);
+    CategoryEntity findByName(String name);
+}
