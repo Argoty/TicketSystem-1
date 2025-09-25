32ed607fa6868a4e35f236a20a2b3200ad6d22e2
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-09-25 18:52:02 -0500

    Elimnado la linea de BCRIPT en el archivo main

--- backend/src/main/java/com/leoalelui/ticketsystem/TicketsystemApplication.java
+++ backend/src/main/java/com/leoalelui/ticketsystem/TicketsystemApplication.java
@@ -8,7 +8,6 @@ public class TicketsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketsystemApplication.class, args);
-		System.out.println(new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder(10).encode("TuPasswordSegura123"));
 	}
 
 }
